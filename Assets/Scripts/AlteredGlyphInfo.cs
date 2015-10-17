484e48f5f00000a085f9ef0393b5be7eea8d7b21
Ted Mader <tlmader@live.com>
2015-10-16 20:41:28 -0500

    Add AlteredGlyphInfo script
    Add AlteredGlyphInfo script

---
+++ Assets/Scripts/AlteredGlyphInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// This class contains information about glyph that are about to be
+// moved after a collapse or new glyph creation event.
+public class AlteredGlyphInfo {
+
+    // List with all the glyphs to be moved
+    private List<GameObject> newGlyph { get; set; }
+    public int MaxDistance { get; set; }
+
+    // Returns distinct list of altered glyphs
+    public IEnumerable<GameObject> AlteredGlyph
+    {
+        get
+        {
+            return newGlyph.Distinct();
+        }
+    }
+
+    // Method to add a new glyph to the list
+    public void AddCandy(GameObject go)
+    {
+        if (!newGlyph.Contains(go))
+        {
+            newGlyph.Add(go);
+        }
+    }
+
+    // Constructor that initializes the private list
+    public AlteredGlyphInfo()
+    {
+        newGlyph = new List<GameObject>();
+    }
+}
---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+[Flags]
+public enum BonusType
+{
+    None,
+    DestroyWholeRowColumn
+}
+
+public enum GameState
+{
+    None,
+    SelectionStarted,
+    Animating
+}
+
+public static class BonusTypeUtilities
+{
+    public static bool ContainsDestroyWholeRowColumn(BonusType bt)
+    {
+        return (bt & BonusType.DestroyWholeRowColumn) == BonusType.DestroyWholeRowColumn;
+    }
+}
